d7bac05643c741f6bc8f5b286b3ca20a44bbcfd7
Abhijit kar <83105184+ABHIJIT-AS@users.noreply.github.com>
2024-07-09 11:26:30 +0000

    datatype conversion done

---
+++ 01_Basics/02_DatatypeConversion.js
@@ -0,0 +1,17 @@
+let score = "33aba";
+
+let valueAtScore = Number(score);
+// console.log(typeof score);
+// console.log(typeof valueAtScore);
+// console.log(valueAtScore);
+
+let isLoggedIn = 2;
+let valueInsideLoggedIn = Boolean(isLoggedIn);
+console.log(typeof valueInsideLoggedIn);
+console.log(valueInsideLoggedIn);
+
+let someNumber = true;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
