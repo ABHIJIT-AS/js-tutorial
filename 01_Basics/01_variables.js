61ff9597154d0b20056b5b870e33fdff77bfe108
Abhijit kar <83105184+ABHIJIT-AS@users.noreply.github.com>
2024-07-09 10:27:03 +0000

    done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1443343;
+let accountName = "Abhijit";
+var accountPassword = "1234";
+accountCity = "Mumbai";
+let accountState;
+accountPassword = "232344"
+accountCity = "Delhi";
+accountName = "Abhi"
+
+console.log(accountCity);
+console.table([accountId, accountName, accountPassword, accountState, accountCity]);
